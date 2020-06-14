062297cc03d920c171bbbf3f94bb20ebfa43820f
james wu <jwu_nyc@yahoo.com>
2020-06-14 01:18:12 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,13 +11,13 @@ end
 def turn(board)
   puts "Please enter 1-9:"
   input=gets.strip
-  input_to_index(index)
+  index=input_to_index(input)
   while valid_move?(board,index)==false
     puts "This (#{index}) is not a valid move. Please input correct move"
     input=gets.strip
-    input_to_index(index)
+    index=input_to_index(input)
   end
-  move(board,converted_number,"X")
+  move(board,index,"X")
   display_board(board)
   #puts 1+converted_number
 end
@@ -59,13 +59,3 @@ end
 #output2 = valid_move?(board,9)
 #puts output
 #puts output2
-puts "Please enter 1-9:"
-input=gets.strip
-index=input_to_index(input)
-while valid_move?(board,index)==false
-  puts "This (#{index}) is not a valid move. Please input correct move"
-  input=gets.strip
-  index=input_to_index(input)
-end
-move(board,index,"X")
-display_board(board)
