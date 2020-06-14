51fd59026fbd655d085404ee4ad6eb59214cd63e
james wu <jwu_nyc@yahoo.com>
2020-06-14 00:11:33 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -32,9 +32,9 @@ end
 
 def valid_move?(board,index)
   if board[index]==" " || board[index]==""
-    true
-  else board[index]=="X" || board[index]=="O"
     false
+  else board[index]=="X" || board[index]=="O"
+    true
   end
 end
 
